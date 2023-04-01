40d23ab7ef2b51fe682bd2588a825417a3839e10
bsaranga <bsaranga@gmail.com>
2023-04-01 06:17:14 +0530

    stringify ast

--- babel-usage/foo.js
+++ babel-usage/foo.js
@@ -1,9 +1,9 @@
 var babel = require('babel-core')
 
-var transformed = babel.transform(`function foo() {
-    console.log('HelloWorld');
+var transformed = babel.transform(`function square(n) {
+    return n*n;
 }`, {
 
 });
 
-console.log(transformed.ast)
+console.log(JSON.stringify(transformed.ast))
